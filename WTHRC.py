a0fb66bab2d5b839a3c837bbcfb8e74a9b2cd553
ramind123 <ramind123@yahoo.co.uk>
2019-01-11 19:43:47 -0800

    Second version of WTHRC

--- WTHRC.py
+++ WTHRC.py
@@ -2,13 +2,44 @@
 my_age = 0
 my_height = 0
 my_weight = 0
-my_gender = "male"
+my_genderV = "male"
 #End of variables.
+#Determine Ratio.
+def result_ratio(height, weight, gender):
+    if gender == "Female":
+        if height <= 46:
+        if height == 47:
+        if height == 48:
+        if height == 49:
+        if height == 50:
+        if height == 51:
+        if height == 52:
+        if height == 53:
+        if height == 54:
+        if height == 55:
+        if height == 56:
+        if height == 57:
+        if height == 58:
+        if height == 59:
+        if height == 60:
+        if height == 61:
+        if height == 62:
+        if height == 63:
+        if height == 64:
+        if height == 65:
+        if height == 66:
+        if height == 67:
+        if height == 68:
+        if height == 69:
+        if height == 70:    
 #Determine Gender.
-my_gender(value):
+def my_gender(value):
     if value == "Male":
         return male
     if value == "Female":
         return female
 #End of gender function.
-print("Are you a Male or Female?")
+value = input("Are you a Male or Female?")
+#Set gender variable.
+my_genderV = my_gender(value)
+print(my_gender)
