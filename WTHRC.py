2b778ce93b491305267f4190dbe8565ef26ef48c
ramind123 <ramind123@yahoo.co.uk>
2019-01-11 19:24:00 -0800

    First version of WTHRC

---
+++ WTHRC.py
@@ -0,0 +1,14 @@
+#Program to calculate Weight to Height ratio.
+my_age = 0
+my_height = 0
+my_weight = 0
+my_gender = "male"
+#End of variables.
+#Determine Gender.
+my_gender(value):
+    if value == "Male":
+        return male
+    if value == "Female":
+        return female
+#End of gender function.
+print("Are you a Male or Female?")
